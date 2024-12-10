5aad4434bf4bd8545fc92c92eaf03ca5dbe84a72
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-12-10 21:05:49 +1100

    created a thesis  model

---
+++ models/thesis.py
@@ -0,0 +1,14 @@
+from init import db, ma
+class Thesis(db.Model):
+    __tablename__="Theses"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    grade = db.Column(db.String(100), nullable=False, unique=True)
+    student_id=db.Column(db.String, db.ForeignKey("students.id"), nullable=False)
+    professor_id=db.Column(db.String, db.ForeignKey("professors.id"), nullable=False)
+class ThesisSchema(ma.Schema):
+    class Meta:
+        fields=("id", "description", "grade", "student_id", "professor_id")
+Thesis_schema = ThesisSchema()
+Theses_schema= ThesisSchema(many=True)
