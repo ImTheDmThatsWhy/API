79fb082d3636800436d74245f35221cba898ea47
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-12-14 14:01:46 +1100

    faculty delete function created

--- controllers/faculties_controller.py
+++ controllers/faculties_controller.py
@@ -13,6 +13,7 @@ def get_faculties():
     data = faculties_schema.dump(faculties_list)
     return data
 
+
 @faculties_bp.route("/<int:faculty_id>")
 def get_faculty(faculty_id):
     stmt = db.select(Faculty).filter_by(id=faculty_id)
@@ -23,15 +24,15 @@ def get_faculty(faculty_id):
     else:
         return {"message": f"faculty with id{faculty_id} not found"}, 404
 
+
 @faculties_bp.route("/", methods=["POST"])
 def create_faculty():
     body_data = request.get_json()
-    new_faculty = Faculty(
-        faculty_name=body_data.get("faculty_name")
-        )
+    new_faculty = Faculty(faculty_name=body_data.get("faculty_name"))
     db.session.add(new_faculty)
     db.session.commit()
     return faculty_schema.dump(new_faculty), 201
+
 
 @faculties_bp.route("/<int:faculty_id>", methods=["PUT", "PATCH"])
 def update_faculty(faculty_id):
@@ -40,10 +41,21 @@ def update_faculty(faculty_id):
         faculty = db.session.scalar(stmt)
         body_data = request.get_json()
         if faculty:
-            faculty.street_number = (body_data.get("faculty_name") or faculty.faculty_name)
+            faculty.faculty_name = body_data.get("faculty_name") or faculty.faculty_name
             db.session.commit()
             return faculty_schema.dump(faculty)
         else:
             return {"message": f"faculty with id{faculty_id} does not exist"}, 404
     except IntegrityError:
         return {"message": "faculty already in system"}, 409
+
+@faculties_bp.route("/<int:faculty_id>", methods=["Delete"])
+def delete_faculty(faculty_id):
+    stmt = db.select(Faculty).filter_by(id=faculty_id)
+    faculty= db.session.scalar(stmt)
+    if faculty:
+        db.session.delete(faculty)
+        db.session.commit()
+        return {"messgage": f"faculty with id{faculty_id} deleted"}
+    else:
+        return {"message": f"faculty with {faculty_id} does not exist"}, 404
