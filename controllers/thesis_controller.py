769b0a8ad3c02176defa591a3c35e80c43d59e08
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-12-17 16:20:30 +1100

    fixed errors in thesis controller

--- controllers/thesis_controller.py
+++ controllers/thesis_controller.py
@@ -1,50 +1,66 @@
 from flask import Blueprint, request
 from init import db
 from models.thesis import Thesis, Thesis_schema, Theses_schema
-theses_bp = Blueprint("theses", __name__, url_prefix="/theses")
+
+thesis_bp = Blueprint("thesis", __name__, url_prefix="/thesis")
 from sqlalchemy.exc import IntegrityError
 
-@theses_bp.route("/")
+
+@thesis_bp.route("/")
 def get_theses():
     stmt = db.select(Thesis)
     theses_list = db.session.scalars(stmt)
     data = Theses_schema.dump(theses_list)
     return data
 
-@theses_bp.route("/<int:thesis_id>")
+
+@thesis_bp.route("/<int:thesis_id>")
 def get_thesis(thesis_id):
     stmt = db.select(Thesis).filter_by(id=thesis_id)
-    thesis=db.session.scalar(stmt)
+    thesis = db.session.scalar(stmt)
     if thesis:
         data = Thesis_schema.dump(thesis)
         return data
     else:
-        return{"message":f"thesis with id {thesis_id}"}
+        return {"message": f"thesis with id {thesis_id}"}
 
-@theses_bp.route("/", methods=["POST"])
+
+@thesis_bp.route("/", methods=["POST"])
 def create_thesis():
-    body_data=request.get_json()
-    new_thesis=Thesis(thesis_name=body_data.get("thesis_name"))
+    body_data = request.get_json()
+    new_thesis = Thesis(
+        name=body_data.get("name"),
+        grade=body_data.get("grade"),
+        student_id=body_data.get("student_id"),
+        degree_level_id=body_data.get("degree_level_id"),
+    )
     db.session.add(new_thesis)
     db.session.commit()
     return Thesis_schema.dump(new_thesis), 201
 
-@theses_bp.route("/<int:thesis_id", methods=["PUT", "PATCH"])
+
+@thesis_bp.route("/<int:thesis_id>", methods=["PUT", "PATCH"])
 def update_thesis(thesis_id):
     try:
-        stmt=db.select(Thesis).filter_by(id=thesis_id)
-        thesis=db.session.scalar(stmt)
-        body_data=request.get_json()
+        stmt = db.select(Thesis).filter_by(id=thesis_id)
+        thesis = db.session.scalar(stmt)
+        body_data = request.get_json()
         if thesis:
-            thesis.thesis_name=body_data.get("thesis_name") or thesis.thesis_name
+            thesis.name = body_data.get("name") or thesis.name
+            thesis.grade = body_data.get("grade") or thesis.grade
+            thesis.student_id = body_data.get("student_id") or thesis.student_id
+            thesis.degree_level_id = (
+                body_data.get("degree_level_id") or thesis.degree_level_id
+            )
             db.session.commit()
             return Thesis_schema.dump(thesis)
         else:
             return {"message": f"thesis with id {thesis_id} does not exist"}, 404
     except IntegrityError:
         return {"message": "thesis already in system"}, 409
-    
-@theses_bp.route("/<int:theses_id>", methods=["Delete"])
+
+
+@thesis_bp.route("/<int:thesis_id>", methods=["Delete"])
 def delete_thesis(thesis_id):
     stmt = db.select(Thesis).filter_by(id=thesis_id)
     thesis = db.session.scalar(stmt)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from controllers.faculties_controller import faculties_bp
 from controllers.students_controller import students_bp
 from controllers.professors_controller import professors_bp
 from controllers.degree_level_controller import degree_levels_bp
+from controllers.thesis_controller import thesis_bp
 
 
 def create_app():
@@ -23,4 +24,5 @@ def create_app():
     app.register_blueprint(students_bp)
     app.register_blueprint(professors_bp)
     app.register_blueprint(degree_levels_bp)
+    app.register_blueprint(thesis_bp)
     return app
