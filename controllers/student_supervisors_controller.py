6d2a5a1f1748c307b35bec2b84111627f1f8c69a
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-12-18 11:37:56 +1100

    added update supervisor student function

--- controllers/student_supervisors_controller.py
+++ controllers/student_supervisors_controller.py
@@ -49,4 +49,17 @@ def create_student_supervisor():
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {"message": f"Field {err.orig.diag.column_name} required "}, 409
 
-       
+@student_supervisors_bp.route("/<int:student_supervisor_id>", methods=["PUT", "PATCH"])
+def update_student_supervisor(student_supervisor_id):
+   
+        stmt = db.select(Student_supervisor).filter_by(id=student_supervisor_id)
+        student_supervisor = db.session.scalar(stmt)
+        body_data = request.get_json()
+
+        if student_supervisor:
+            student_supervisor.student_id = body_data.get("student_id")
+            student_supervisor.professor_id = body_data.get("professor_id")
+            db.session.commit()
+            return student_supervisor_schema.dump(student_supervisor)
+        else:
+            return {"message": f"Student with id {student_supervisor_id} does not exist"}, 404
